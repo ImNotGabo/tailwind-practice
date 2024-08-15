f70646f0c5b10f00b65106138ec685e08f7fb4c6
Gabriel Duarte <gabriel.duarte18@outlook.es>
2024-08-14 23:25:06 -0400

    Upd

--- src/components/InputComponent.jsx
+++ src/components/InputComponent.jsx
@@ -2,12 +2,12 @@ function InputComponent() {
   return (
     <div>
       <form className='flex flex-col items-center justify-center h-screen gap-3'>
-        <label htmlFor='fullName' name='FullName' className='text-gray-700 text-sm selec font-bold '>
+        <label htmlFor='fullName' className='text-gray-700 text-sm selec font-bold '>
           Full Name
         </label>
         <input
           type='text'
-          name='FullName'
+          id='fullName'
           className='placeholder-blue-300 border border-gray-300 rounded-lg py-2 px-4 w-4/6 shadow-md focus:bg-blue-100 text-black'
           placeholder='Enter your full name'
         />
